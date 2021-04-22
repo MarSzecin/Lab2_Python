df63a691f85bbfbb64ae3033fcb60a387941a41a
MarcinSzewczyk99 <marcinekszewczyk90@gmail.com>
2021-04-22 10:09:22 +0200

    Zrobione z zad19

---
+++ zadanie15.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 09:20:02 2021
+
+@author: marci
+"""
+
+import numpy as np
+
+
+def zadanie15(x):
+    if x<0:
+        return np.sin(x)
+    else:
+        return np.sqrt(x)
